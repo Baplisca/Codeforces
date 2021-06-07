b560a618ce994092cef54339768516844d7cc14c
“Baplisca” <yokota0327@gmail.com>
2021-06-07 23:59:51 +0900

    Codeforces #1154B: Make Them Equal

---
+++ 1154/B.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define rep1(i, n) for (int i = 1; i < n; ++i)
+#define exrep(i, a, b) for (ll i = a; i < b; i++)
+#define out(x) cout << x << endl
+#define EPS (1e-7)
+#define gearup                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<char>> vvc;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<double>> vvd;
+typedef vector<vector<string>> vvs;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+ll MOD = 998244353;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template <class T>
+inline bool chmin(T &a, T b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T &a, T b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+void debug(T v)
+{
+    rep(i, v.size()) cout << v[i] << " ";
+    cout << endl;
+}
+ll pcount(ll x) { return __builtin_popcountll(x); }
+const ll dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//std::ifstream in("input.txt");
+//std::cin.rdbuf(in.rdbuf());
+
+signed main()
+{
+    gearup;
+    int n;
+    cin >> n;
+    vl a(n);
+    set<int> s;
+    rep(i, n)
+    {
+        cin >> a[i];
+        s.insert(a[i]);
+    }
+    sort(all(a));
+    if (s.size() == 1)
+        out(0);
+    else if (s.size() == 2)
+    {
+        int diff = a[n - 1] - a[0];
+        if (diff & 1)
+            out(diff);
+        else
+            out(diff / 2);
+    }
+    else if (s.size() == 3)
+    {
+        int mid;
+        rep(i, n) if (a[i] != a[0] && a[i] != a[n - 1]) mid = a[i];
+        int diff1 = a[n - 1] - mid;
+        int diff2 = mid - a[0];
+        if (diff1 == diff2)
+            out(diff1);
+        else
+            out(-1);
+    }
+    else
+        out(-1);
+}
