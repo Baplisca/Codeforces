5453a4f74bc23a3250caafd753cb089778ddfb52
“Baplisca” <yokota0327@gmail.com>
2021-06-07 23:59:42 +0900

    Codeforces #1494B: Berland Crossword

---
+++ 1494/B.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define rep1(i, n) for (int i = 1; i < n; ++i)
+#define exrep(i, a, b) for (ll i = a; i < b; i++)
+#define out(x) cout << x << endl
+#define EPS (1e-7)
+#define gearup                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<char>> vvc;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<double>> vvd;
+typedef vector<vector<string>> vvs;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+ll MOD = 998244353;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template <class T>
+inline bool chmin(T &a, T b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T &a, T b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+void debug(T v)
+{
+    rep(i, v.size()) cout << v[i] << " ";
+    cout << endl;
+}
+ll pcount(ll x) { return __builtin_popcountll(x); }
+const ll dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//std::ifstream in("input.txt");
+//std::cin.rdbuf(in.rdbuf());
+
+signed main()
+{
+    gearup;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int u, r, d, l;
+        cin >> u >> r >> d >> l;
+        bool is_res = false;
+        rep(i, 1 << 4)
+        {
+            int U, R, D, L;
+            U = R = D = L = 0;
+            rep(j, 4)
+            {
+                if ((i >> j) & 1)
+                {
+                    if (j == 0)
+                        U++, L++;
+                    if (j == 1)
+                        U++, R++;
+                    if (j == 2)
+                        D++, L++;
+                    if (j == 3)
+                        D++, R++;
+                }
+            }
+            if (U <= u && u - U <= n - 2 && D <= d && d - D <= n - 2 && L <= l && l - L <= n - 2 && R <= r && r - R <= n - 2)
+                is_res = true;
+        }
+        if (is_res)
+            out("YES");
+        else
+            out("NO");
+    }
+}
